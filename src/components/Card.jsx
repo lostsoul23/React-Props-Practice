9e42d1d60e03ac2cbca410915a4552fe79c6b43e
Anirban Haldar <99974355+lostsoul23@users.noreply.github.com>
2022-07-05 03:47:12 +0530

    Initial commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+// can use any other name too: Eg: porperties, Props, xyz, etc...
+function Card(props) {
+  return (
+    <div>
+      <div className="card">
+        <div className="top">
+          <h2 className="name">{props.name}</h2>
+          <img className="circle-img" src={props.img} alt="avatar_img" />
+        </div>
+        <div className="bottom">
+          <p className="info">{props.tel}</p>
+          <p className="info">{props.email}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
